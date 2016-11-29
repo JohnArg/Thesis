d192f8042efcf4e9a8234e041748cdbe7e0d9e15
John Argyroulis <johnnyargi@yahoo.gr>
2016-11-29 20:40:09 +0200

    max-min backend almost complete.

--- Server/Algorithms/max_min_clusters.js
+++ Server/Algorithms/max_min_clusters.js
@@ -14,23 +14,28 @@ var MaxMinFactory = function(){
 var MaxMinClusters = function(){
     var that = this;
     that.solution = {
-        "final_resutls" : [],
-        "floodmax" : {},
-        "floodmin" : {}
+        "floodmax" : [],
+        "floodmin" : [],
+        "clusters" : [],
+        "extra_notes" : ""
     };
     that.calculateMaxMixClusters = function(network, d){
         //the d value is assosiated with the hops in the algorithm
         _convertToMaxMinNodes(network);
+        _roundSimulator(network , d);
+        _finalWinners(network, d, solution);
+        _constructSolution(network, solution);
+        return that.solution;
     };
 }
 
 //Adds data to network nodes
 var _convertToMaxMinNodes = function(network){
     for(var i=0; i<network.nodes.length; i++){
-        network.nodes[i].winner = 0;
         network.nodes[i].floodmax = [];
         network.nodes[i].floodmin = [];
         network.nodes[i].received = [];
+        network.nodes[i].finalWinner = {};
     }
 }
 
@@ -41,9 +46,135 @@ var _clearReceiveList = function(network){
     }
 }
 
+//Broadcast winner node simulation
+var _broadcastWinner = function(sender, winner_id, network){
+    var tempNode;
+    for(var i=0; i<sender.neighbors.length; i++){
+        tempNode = netOperator.returnNodeById(sender.neighbors[i]);
+        tempNode.received.push({"sender" : sender.id, "winner" : winner_id});
+    }
+}
+
+//Evaluate winner
+var _evaluateWinner = function(node, floodType){
+    //find the max "winner"
+    if(floodType == "max"){
+        var max = node.received[0];
+        for(var j=1; j<node.received.length; j++){
+            if(node.received[j]["winner"] > max["winner"]){
+                max = node.received[j];
+            }
+        }
+        node.floodmax.push(max);
+    }
+    else{
+        var min = node.received[0];
+        for(var j=1; j<node.received.length; j++){
+            if(node.received[j]["winner"] < min["winner"]){
+                min = node.received[j];
+            }
+        }
+        node.floodmin.push(min);
+    }
+    node.received = [];
+}
+
 //Initial procedure
 var _procedureInit = function(network){
-    
+    for(var i=0; i<network.nodes.length; i++){
+        _broadcastWinner(network.nodes[i], network.nodes[i].id, network);
+    }
+    for(var i=0; i<network.nodes.length; i++){
+        _evaluateWinner(network.nodes[i], "max");
+    }
+}
+
+//Round simulator
+var _roundSimulator = function(network , d){
+    //floodmax
+    for(var j=0; j<d; j++){
+        if(j == 0){
+            _procedureInit(network);
+        }
+        else{
+            for(var i=0; i<network.nodes.length; i++){
+                //broadcast previous round winner
+                _broadcastWinner(network.nodes[i], network.nodes[i].floodmax[j-1]["winner"], network);
+            }
+            for(var i=0; i<network.nodes.length; i++){
+                _evaluateWinner(network.nodes[i], "max");
+            }
+        }
+    }
+    //floodmin
+    for(var j=0; j<d; j++){
+        if(j == 0){
+            for(var i=0; i<network.nodes.length; i++){
+                //broadcast previous round winner
+                _broadcastWinner(network.nodes[i], network.nodes[i].floodmax[d-1]["winner"], network);
+            }
+            for(var i=0; i<network.nodes.length; i++){
+                _evaluateWinner(network.nodes[i], "min");
+            }
+        }
+        else{
+            for(var i=0; i<network.nodes.length; i++){
+                _broadcastWinner(network.nodes[i], network.nodes[i].floodmin[j-1]["winner"], network);
+            }
+            for(var i=0; i<network.nodes.length; i++){
+                _evaluateWinner(network.nodes[i], "min");
+            }
+        }
+    }
+}
+
+//Return the intersection of floodmax and floodmin
+var _returnFloodIntersection = function(node){
+    intersect = [];
+    for( var i=0; i< node.floodmax.length; i++){
+        for(var j=0; j<node.floodmin.length; j++){
+            if(node.floodmax[i]["winner"] == node.floodmin[j]["winner"]){
+                intersect.push(node.floodmax[i]);
+            }
+        }
+    }
+    return intersect;
+}
+
+//Returns the senders of the winner id from the node pairs
+var _returnWinnerSenders = function(comparisonType, nodePairs){
+    var senders = [];
+    for(var i=0; i<nodePairs.length; i++){
+
+    }
+}
+
+//Will decide the final winners of the execution
+var _finalWinners = function(network, d, solution){
+    //Use rule 1,2 and 3 to define th winners 
+    for(var i=0; i<network.nodes[i].length; i++){
+        //if i have my id as winner in floodmin end
+        if(network.nodes[i].id == network.nodes[i].floodmin[d-1]["winner"]){
+            network.nodes[i].finalWinner = network.nodes[i].floodmin[d-1];
+        }
+        else{
+            var nodePairs = _returnFloodIntersection(network.nodes[i]);
+            if(nodePairs.length > 0){
+                network.nodes[i].finalWinner = _.min(nodePairs, function(elem){return elem["winner"];});
+            }
+            else{
+                network.nodes[i].finalWinner = _.max(network.nodes[i].floodmax, function(elem){return elem["winner"];});
+            }
+        }
+    }
+    //Nodes will send their id to the cluster they selected. If another cluster is found in the path
+    //then the sender node must change his previous clusterhead to this one. Below we simulate this behaviour.
+
+}
+
+//Put the results to the solution object
+var _constructSolution = function(solution){
+
 }
 
 module.exports.newMaxMinObject = MaxMinFactory;
--- Server/Algorithms/steps.js
+++ Server/Algorithms/steps.js
@@ -1,16 +1,11 @@
 /*
 In this application we need to describe to the user
 how an algorithm works, step by step.
-This module will create "Step" objects that will describe
-in text the steps of an algorithm. Its purpose is to
-hold all the information needed, so when the user clicks
-to a step panel, the current state of the program, will be 
-shown.
-
-Numbered explanations referenced from the code:
-[1]
-	To be parsable from the client's viewManager.js, 
-	CDS algorithms should insert (key -> "dominators" : value -> dominator list).
+This module will create "Solution" and Step" objects that will 
+describe in text the steps of an algorithm. Its purpose is to
+hold all the information needed, so the user can 
+see the solution as a whole or click on individual steps 
+and see the result of that step.
 */
 
 //This object will hold the information for this step of execution
@@ -21,10 +16,10 @@ var Step = function(){
 
 //This object will contain info and the steps executed in a specific solution
 var Solution = function(){
-	this.text = "";	//optional general description
-	this.steps = [];
+	this.text = "";	//optional extra text
+	this.steps = []; //the steps of the solution
 	var stepList = this.steps;
-	this.result = {}; //see [1] above
+	this.result = {}; //see [1] above. Can hold an object showing the result so far
 	this.createStep = function(){
 		var step = new Step();
 		stepList.push(step);
